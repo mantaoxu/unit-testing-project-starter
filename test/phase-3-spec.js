83858c6d4ff630940c787565816685d883c21571
brad <bradsherwood24816@gmail.com>
2021-04-16 13:25:00 -0700

    phase 3 complete

--- test/phase-3-spec.js
+++ test/phase-3-spec.js
@@ -10,48 +10,54 @@ describe('Word', function () {
       word = new Word('Friday')
 
       expect(word).to.have.property('word');
-    
+
     })
      it('should set the "word" property when a new word is created', function() {
       //Arrange & Act
       let word;
       word = new Word('Friday');
 
-      
+
       //Assert
        expect(word.word).to.eql('Friday');
-      
+
     })
   })
   describe('removeVowels function', function () {
     it('should return a the word with all vowels removed', function() {
       //Arrange
-      
+      let word = new Word("Friday");
+
       //Act
-      
+      let result = word.removeVowels()
+
       //Assert
-       expect.fail('Remove this expect.fail and replace it with your test');
+       expect(result).to.eql("Frdy");
     })
   })
   describe('removeConsonants function', function () {
 })
   it('should return the word with the consonants removed', function() {
     //Arrange
-    
+    let word = new Word("Friday");
+
     //Act
-    
+    let result = word.removeConsonants();
+
     //Assert
-     expect.fail('Remove this expect.fail and replace it with your test');
+     expect(result).to.eql("ia");
   })
   describe('pigLatin function', function () {
 })
   it('should return the word converted to pig latin', function() {
     //Arrange
-    
+    let word = new Word("Friday");
+
     //Act
-    
+    let result = word.pigLatin();
+
     //Assert
-     expect.fail('Remove this expect.fail and replace it with your test');
-    
+     expect(result).to.eql("idayFray");
+
   })
 })
